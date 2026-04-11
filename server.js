97b139f8ad7ac68c33802b03f92b4f1baf6a3055
Zeck51 <azkiyafauzan@gmail.com>
2026-04-11 22:41:11 +0700

    Change static file serving to 'public' directory
    Updated static file serving to use 'public' directory for HTML files.

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname)));
+// Serve dari folder public
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/admin', (req, res) => {
-  res.sendFile(path.join(__dirname, 'admin.html'));
+  res.sendFile(path.join(__dirname, 'public', 'admin.html'));
 });
 
 const PORT = process.env.PORT || 3000;
